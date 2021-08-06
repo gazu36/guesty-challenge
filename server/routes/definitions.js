b0771aeb009328ee9c9c7da07a78e14e4f1f31d5
Guy Azulay <guyazulay36@gmail.com>
2021-08-06 19:09:06 +0300

    parse iso dt

--- server/routes/definitions.js
+++ server/routes/definitions.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const { param } = require('express-validator');
+const { DateTime } = require('luxon');
+
 const validateNoErrors = require('../validators/errors-validate');
 
 const router = express.Router();
@@ -15,7 +17,10 @@ router.get('/:datetime',
     validateNoErrors,
     (req, res) => {
     const {datetime} = req.params;
-    return res.json(`Hello World ${datetime}`);
+    console.log(datetime);
+    const dt = DateTime.fromISO(datetime, {setZone: true});
+    
+    return res.json(`Hello World ${dt.weekdayShort} (${dt.weekday % 7}) - ${dt.hour}:00 @ ${dt.zoneName}`);
 });
 
 module.exports = router;
